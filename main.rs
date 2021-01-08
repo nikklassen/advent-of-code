b8724c195fe347b504d4c28a7997bcf7321abfb8
Nik Klassen <nikklassen@users.noreply.github.com>
2021-01-07 22:54:18 -0500

    Day 17

--- main.rs
+++ main.rs
@@ -22,15 +22,16 @@ mod day13;
 mod day14;
 mod day15;
 mod day16;
+mod day17;
 
 use std::time::Instant;
 
 fn main() {
     let mut start = Instant::now();
-    println!("part1: {}", day16::part1());
+    println!("part1: {}", day17::part1());
     println!("elapsed: {:?}", Instant::now().duration_since(start));
 
     start = Instant::now();
-    println!("part2: {}", day16::part2());
+    println!("part2: {}", day17::part2());
     println!("elapsed: {:?}", Instant::now().duration_since(start));
 }
