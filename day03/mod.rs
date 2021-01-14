9c83f301e50d039ace73d2f46c1a569d6f92018c
Nik Klassen <nikklassen@users.noreply.github.com>
2021-01-13 21:01:03 -0500

    Optimize Day 3
    test day03::tests::bench_part_1 ... bench:      40,071 ns/iter (+/- 17,150)
    test day03::tests::bench_part_2 ... bench:      40,650 ns/iter (+/- 11,392)

--- day03/mod.rs
+++ day03/mod.rs
@@ -2,17 +2,28 @@ use crate::utils;
 
 use std::iter;
 
+lazy_static! {
+    static ref INPUT: Vec<String> = utils::read_input_lines("day03");
+}
+
 fn read_map() -> Vec<Vec<bool>> {
-    let input = utils::read_input_lines("day03");
-    input
-        .iter()
-        .map(|line| line.chars().map(|c| c == '#').collect())
-        .collect()
+    let mut map = vec![vec![false; INPUT[0].len()]; INPUT.len()];
+    for i in 0..INPUT.len() {
+        let row = &INPUT[i];
+        for j in 0..row.len() {
+            if INPUT[i].as_bytes()[j] == b'#' {
+                map[i][j] = true;
+            }
+        }
+    }
+    map
 }
 
 fn slide(m: &[Vec<bool>], slope: (usize, usize)) -> usize {
     let height = m.len();
     let width = m[0].len();
+    // Surprisingly this is basically the same performance as the loop-based
+    // version.
     iter::successors(Some((0, 0)), move |pos| {
         let y = pos.1 + slope.1;
         if y < height {
@@ -21,8 +32,8 @@ fn slide(m: &[Vec<bool>], slope: (usize, usize)) -> usize {
             None
         }
     })
-    .map(move |pos| m[pos.1][pos.0 % width])
-    .fold(0, |acc, v| if v { acc + 1 } else { acc })
+    .map(move |pos| if m[pos.1][pos.0 % width] { 1 } else { 0 })
+    .sum()
 }
 
 pub fn part1() -> usize {
@@ -34,4 +45,31 @@ pub fn part2() -> usize {
     let m = read_map();
     let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
     slopes.iter().map(|&slope| slide(&m, slope)).product()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn run_part1() {
+        assert_eq!(part1(), 191);
+    }
+
+    #[test]
+    fn run_part2() {
+        assert_eq!(part2(), 1478615040);
+    }
+
+    #[bench]
+    fn bench_part_1(b: &mut Bencher) {
+        b.iter(part1);
+    }
+
+    #[bench]
+    fn bench_part_2(b: &mut Bencher) {
+        b.iter(part2);
+    }
 }
