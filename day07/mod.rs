a43b3c68f7f4caabb34486a353a60ecc03c34982
Nik Klassen <nikklassen@users.noreply.github.com>
2021-01-14 22:48:54 -0500

    Optimize Day 7
    test day07::tests::bench_part_1 ... bench:   1,462,465 ns/iter (+/- 724,432)
    test day07::tests::bench_part_2 ... bench:   1,380,245 ns/iter (+/- 718,508)
    
    to
    
    test day07::tests::bench_part_1 ... bench:     306,407 ns/iter (+/- 75,892)
    test day07::tests::bench_part_2 ... bench:     247,241 ns/iter (+/- 118,756)

--- day07/mod.rs
+++ day07/mod.rs
@@ -1,123 +1,138 @@
-use std::collections::HashMap;
+use crate::utils::{self, *};
 
-use regex::Regex;
-
-use crate::utils;
-
-#[derive(PartialEq, Eq, Hash, Clone)]
-struct Bag {
-    attr: String,
-    color: String,
+lazy_static! {
+    static ref INPUT: Vec<String> = utils::read_input_lines("day07");
 }
 
+type Bag = usize;
+
+#[derive(Default, Clone)]
 struct BagRule {
     count: i32,
     bag: Bag,
 }
 
+#[derive(Default, Clone)]
 struct LuggageRule {
     bag: Bag,
     inner_bag_rules: Vec<BagRule>,
 }
 
-lazy_static! {
-    static ref LUGGAGE_REGEX: Regex = Regex::new(r"^(?:(\d+) )?(\w+) (\w+) bags?\.?$").unwrap();
+type EnumMap<'a> = HashMap<(&'a str, &'a str), usize>;
+
+fn map_enum<'a>(m: &mut EnumMap<'a>, key: (&'a str, &'a str)) -> usize {
+    let n = m.len();
+    *m.entry(key).or_insert(n)
 }
 
-fn parse_luggage_rule<S: AsRef<str>>(s: S) -> LuggageRule {
-    let parts = s.as_ref().split(" contain ").collect::<Vec<_>>();
-    let captures = LUGGAGE_REGEX.captures(parts[0]).unwrap();
-    let attr = captures[2].to_string();
-    let color = captures[3].to_string();
+fn parse_luggage_rule<'a>(bag_map: &mut EnumMap<'a>, s: &'a str) -> LuggageRule {
+    let mut parts = s.split(' ');
+    let bag = map_enum(bag_map, (parts.next().unwrap(), parts.next().unwrap()));
+    parts.next();
+    parts.next();
+
     let mut inner_bag_rules = Vec::new();
-    if parts[1] != "no other bags." {
-        for inner_part in parts[1].split(", ") {
-            let captures = LUGGAGE_REGEX.captures(inner_part).unwrap();
-            inner_bag_rules.push(BagRule {
-                count: captures[1].to_string().parse().unwrap(),
-                bag: Bag {
-                    attr: captures[2].to_string(),
-                    color: captures[3].to_string(),
-                },
-            });
+    while let Some(n) = parts.next() {
+        if n == "no" {
+            break;
         }
+        let count = n.parse().unwrap();
+        let bag = map_enum(bag_map, (parts.next().unwrap(), parts.next().unwrap()));
+        inner_bag_rules.push(BagRule { count, bag });
+
+        parts.next();
     }
     LuggageRule {
-        bag: Bag { attr, color },
+        bag,
         inner_bag_rules,
     }
 }
 
-fn read_rules() -> HashMap<Bag, LuggageRule> {
-    utils::read_input_lines("day07")
-        .into_iter()
-        .map(parse_luggage_rule)
-        .map(|lr| (lr.bag.clone(), lr))
-        .collect()
+fn read_rules() -> (Vec<LuggageRule>, EnumMap<'static>) {
+    let mut bag_map = EnumMap::with_capacity(INPUT.len());
+    let mut all_bags = vec![LuggageRule::default(); INPUT.len()];
+    for s in INPUT.iter() {
+        let lr = parse_luggage_rule(&mut bag_map, s.as_str());
+        let bag = lr.bag;
+        all_bags[bag] = lr;
+    }
+    (all_bags, bag_map)
 }
 
 fn has_bag(
     memo: &mut HashMap<Bag, bool>,
-    rules: &HashMap<Bag, LuggageRule>,
-    bag: &Bag,
-    search_bag: &Bag,
+    rules: &[LuggageRule],
+    bag: Bag,
+    search_bag: Bag,
 ) -> bool {
-    if *bag == *search_bag {
+    if let Some(&v) = memo.get(&bag) {
+        return v;
+    }
+    if bag == search_bag {
         return true;
     }
-    if let Some(v) = memo.get(bag) {
-        return *v;
-    }
-    let v = rules
-        .get(bag)
-        .unwrap()
+    let v = rules[bag]
         .inner_bag_rules
         .iter()
-        .any(|b| has_bag(memo, rules, &b.bag, search_bag));
-    memo.insert(bag.clone(), v);
+        .any(|b| has_bag(memo, rules, b.bag, search_bag));
+    memo.insert(bag, v);
     v
 }
 
 pub fn part1() -> usize {
-    let shiny_gold_bag = Bag {
-        attr: "shiny".to_string(),
-        color: "gold".to_string(),
-    };
-    let rules = read_rules();
+    let (rules, bag_map) = read_rules();
+    let shiny_gold_bag = *bag_map.get(&("shiny", "gold")).unwrap();
     let mut c = 0;
     let mut memo = HashMap::new();
-    for (b, _) in rules.iter() {
-        if *b == shiny_gold_bag {
-            continue;
-        }
-        if has_bag(&mut memo, &rules, b, &shiny_gold_bag) {
+    for lr in rules.iter() {
+        if has_bag(&mut memo, &rules, lr.bag, shiny_gold_bag) {
             c += 1;
         }
     }
-    c
+    // Gold bag counts itself.
+    c - 1
 }
 
-fn count_bags(
-    memo: &mut HashMap<Bag, usize>,
-    rules: &HashMap<Bag, LuggageRule>,
-    bag: &Bag,
-) -> usize {
-    if let Some(v) = memo.get(bag) {
+fn count_bags(memo: &mut HashMap<Bag, usize>, rules: &[LuggageRule], bag: Bag) -> usize {
+    if let Some(v) = memo.get(&bag) {
         return *v;
     }
     let mut tot = 0;
-    for inner_bag_rule in rules.get(bag).unwrap().inner_bag_rules.iter() {
-        tot += (inner_bag_rule.count as usize) * (1 + count_bags(memo, rules, &inner_bag_rule.bag))
+    for inner_bag_rule in rules[bag].inner_bag_rules.iter() {
+        tot += (inner_bag_rule.count as usize) * (1 + count_bags(memo, rules, inner_bag_rule.bag))
     }
-    memo.insert(bag.clone(), tot);
+    memo.insert(bag, tot);
     tot
 }
 
 pub fn part2() -> usize {
-    let rules = read_rules();
-    let shiny_gold_bag = Bag {
-        attr: "shiny".to_string(),
-        color: "gold".to_string(),
-    };
-    count_bags(&mut HashMap::new(), &rules, &shiny_gold_bag)
+    let (rules, bag_map) = read_rules();
+    let shiny_gold_bag = *bag_map.get(&("shiny", "gold")).unwrap();
+    count_bags(&mut HashMap::new(), &rules, shiny_gold_bag)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn run_part1() {
+        assert_eq!(part1(), 246);
+    }
+
+    #[test]
+    fn run_part2() {
+        assert_eq!(part2(), 2976);
+    }
+
+    #[bench]
+    fn bench_part_1(b: &mut Bencher) {
+        b.iter(part1);
+    }
+
+    #[bench]
+    fn bench_part_2(b: &mut Bencher) {
+        b.iter(part2);
+    }
 }
