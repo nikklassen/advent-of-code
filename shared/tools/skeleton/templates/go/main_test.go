2708c7ce8f7d73fb6e025436551a75dca16f6a41
Nik Klassen <nikklassen@users.noreply.github.com>
2023-12-18 20:02:35 -0500

    Use Benchmark in skeleton test file

--- shared/tools/skeleton/templates/go/main_test.go
+++ shared/tools/skeleton/templates/go/main_test.go
@@ -2,18 +2,22 @@ package main
 
 import "testing"
 
-func TestPart1(t *testing.T) {
-	want := 0
-	got := part1(input)
-	if got != want {
-		t.Errorf("part1 got %d, want %d", got, want)
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		want := 0
+		got := part1(input)
+		if got != want {
+			b.Fatalf("part1 got %d, want %d", got, want)
+		}
 	}
 }
 
-// func TestPart2(t *testing.T) {
-// 	want := 0
-// 	got := part2(input)
-// 	if got != want {
-// 		t.Errorf("part2 got %d, want %d", got, want)
-// 	}
+// func BenchmarkPart2(b *testing.B) {
+//   for i := 0; i < b.N; i++ {
+//     want := 0
+//     got := part2(input)
+//     if got != want {
+//       b.Fatalf("part2 got %d, want %d", got, want)
+//     }
+//   }
 // }
