5430303ba4bdb5a5690295693596df6c8683d8d7
Nik Klassen <nikklassen@users.noreply.github.com>
2023-12-20 09:35:09 -0500

    wip

--- 2023/day19/main.go
+++ 2023/day19/main.go
@@ -8,10 +8,11 @@ import (
 	"github.com/nikklassen/advent-of-code/shared/utils/aocmaps"
 	"github.com/nikklassen/advent-of-code/shared/utils/aocslices"
 	"github.com/nikklassen/advent-of-code/shared/utils/aocstrings"
+	"golang.org/x/exp/maps"
 )
 
 var (
-	//go:embed test_input.txt
+	//go:embed input.txt
 	input string
 )
 
@@ -21,20 +22,37 @@ type workflow struct {
 }
 
 type rule struct {
-	category rune
-	op       rune
+	category string
+	op       string
 	value    int
 	dest     string
 }
 
+func (r rule) test(p part) bool {
+	switch r.op {
+	case "":
+		return true
+	case "<":
+		return p[r.category] < r.value
+	case ">":
+		return p[r.category] > r.value
+	default:
+		panic("invalid op")
+	}
+}
+
 func parseRule(line string) rule {
-	line, dest, _ := strings.Cut(line, ":")
-	category := line[0]
-	op := line[1]
+	line, dest, ok := strings.Cut(line, ":")
+	if !ok {
+		return rule{dest: line}
+	}
+	idx := strings.IndexAny(line, "><")
+	category := line[:idx]
+	op := line[idx : idx+1]
 	return rule{
-		category: rune(category),
-		op:       rune(op),
-		value:    aocstrings.MustAtoi(line[2:]),
+		category: category,
+		op:       op,
+		value:    aocstrings.MustAtoi(line[idx+1:]),
 		dest:     dest,
 	}
 }
@@ -46,16 +64,52 @@ func parseWorkflow(line string) workflow {
 	return workflow{label, rules}
 }
 
+type part map[string]int
+
+func runWorkflow(p part, wm map[string]workflow) bool {
+	curr := wm["in"]
+	for {
+		var next string
+		for _, r := range curr.rules {
+			if ok := r.test(p); ok {
+				if r.dest == "R" {
+					return false
+				} else if r.dest == "A" {
+					return true
+				}
+				next = r.dest
+				break
+			}
+		}
+		if next == "" {
+			panic("not found")
+		}
+		curr = wm[next]
+	}
+}
+
 func part1(input string) int {
 	paras := aocstrings.Paragraphs(input)
 	workflows := aocslices.Map(aocstrings.Lines(paras[0]), parseWorkflow)
 	workflowMap := aocmaps.FromSliceFunc(workflows, func(w workflow) string {
 		return w.label
 	})
-	for _, part := range paras[1] {
-
+	var parts []part
+	for _, line := range aocstrings.Lines(paras[1]) {
+		attrs := strings.Split(line[1:len(line)-1], ",")
+		p := part{}
+		for _, attr := range attrs {
+			p[attr[:1]] = aocstrings.MustAtoi(attr[2:])
+		}
+		parts = append(parts, p)
 	}
-	return 0
+	var tot int
+	for _, p := range parts {
+		if runWorkflow(p, workflowMap) {
+			tot += aocslices.Sum(maps.Values(p))
+		}
+	}
+	return tot
 }
 
 // func part2(input string) int {
